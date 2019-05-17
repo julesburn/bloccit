64843135575186ae172134d843fc9598e9df186a
Christine <cmbenson88@gmail.com>
2019-05-17 15:50:19 -0500

    Checkpoint-3 local commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 const routeConfig = require("./config/route-config.js");
 
 routeConfig.init(app);
-=======
-const express = require("express");
-const app = express();
 
- app.use("/", (req, res, next) => {
-   res.send("Welcome to Bloccit")
- });
-
-module.exports = app;
->>>>>>> 5346e774fcc009600ea9d7c3bdcd3180a4d8bdf9
