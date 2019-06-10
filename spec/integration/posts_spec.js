7515fb6e42553bbc95549e59d5fb9e0603e45e6f
Christine <cmbenson88@gmail.com>
2019-06-10 11:41:26 -0500

    Checkpoint 10 complete

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -78,6 +78,32 @@ describe("routes : posts", () => {
          }
        );
      });
+     it("should not create a new post that fails validations", (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options,
+        (err, res, body) => {
+
+          Post.findOne({where: {title: "a"}})
+          .then((post) => {
+              expect(post).toBeNull();
+              done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+
  
   });
 
@@ -140,7 +166,8 @@ describe("GET /topics/:topicId/posts/:id/edit", () => {
         const options = {
           url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
           form: {
-            title: "Snowman Building Competition"
+            title: "Snowman Building Competition",
+            body: "I really enjoy the funny hates on them."
           }
         };
         request.post(options,
