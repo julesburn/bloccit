ac22cbdacfffcd003313250c526fe8d876168b0c
Christine <cmbenson88@gmail.com>
2019-06-19 16:11:39 -0500

    Stash changes Assignment-12

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -1,11 +1,12 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/topics";
-const User = require("../../src/db/models").User;
+
 
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("routes : posts", () => {
 
