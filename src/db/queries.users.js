e1ec144570dfc5271d0de8e3fecea64ddbdc8d31
Christine <cmbenson88@gmail.com>
2019-06-27 23:31:43 -0500

    Completes Checkpoint 16

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,4 +1,6 @@
 const User = require("./models").User;
+const Post = require("./models").Post;
+const Comment = require("./models").Comment;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
@@ -18,6 +20,37 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
-  }
+  },
+
+  getUser(id, callback){
+
+       let result = {};
+       User.findById(id)
+       .then((user) => {
+    
+         if(!user) {
+           callback(404);
+         } else {
+    
+           result["user"] = user;
+    
+           Post.scope({method: ["lastFiveFor", id]}).findAll()
+           .then((posts) => {
+    
+             result["posts"] = posts;
+    
+             Comment.scope({method: ["lastFiveFor", id]}).findAll()
+             .then((comments) => {
+    
+               result["comments"] = comments;
+               callback(null, result);
+             })
+             .catch((err) => {
+               callback(err);
+             })
+           })
+         }
+       })
+     }
 
 }
